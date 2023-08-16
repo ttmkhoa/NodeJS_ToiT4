ea0a6961417478f7339c24b865dea3d80c306081
Khoa Tran <45887510+ttmkhoa@users.noreply.github.com>
2023-08-16 19:36:13 +0700

    hello world with express

---
+++ hello.js
@@ -0,0 +1,14 @@
+const express = require('express')
+
+const app = express();
+
+const port = 3000;
+
+app.get('/', function(request, response){
+    response.send('Hello World from express framwwork')
+
+});
+
+app.listen(port, function(){
+    console.log('Server listening on http://localhost:'+port);
+});
